b870d49c9164c687f0183cbf113c30db9c5e5723
JoseCrescencio <jcrescencio@csumb.edu>
2019-02-18 00:05:37 -0800

    Finished twitter part 1

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Jose Crescencio on 2/17/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
